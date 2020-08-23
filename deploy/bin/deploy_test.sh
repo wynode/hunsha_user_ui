269cf88367beafc36c7745f1f4344d32ae1133e7
wynode <bugeikela@gmail.com>
2020-08-23 18:06:26 +0800

    feat: 修改deploy

--- deploy/bin/deploy_test.sh
+++ deploy/bin/deploy_test.sh
@@ -2,4 +2,4 @@ yarn install &&
 # yarn test:unit &&
 yarn run build:test &&
 # scp -r deploy/nginx/nginx.test.conf wyme@hongkong:/home/wyme/project/hunsha_ui &&
-scp -r dist wyme@hongkong:/home/wyme/project/hunsha_user_ui
+scp -r dist wynode@nanjing:/home/wynode/project/hunsha_user_ui
